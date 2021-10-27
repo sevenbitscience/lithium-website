9f2c043b23abc9bf255555c0d8619dc941c25a37
sevenbitscience <36937340+sevenbitscience@users.noreply.github.com>
2021-10-27 16:05:08 -0400

    Fixed element box

--- main.js
+++ main.js
@@ -1,23 +1,17 @@
-$(document).ready(function () {
-    $(".elemname").hide(0);
-    
-    var offset = $(".header").offset();
-    var top = offset.top;
-    var left = offset.left;
+$(document).ready(function () {    
     var headerclosed = false;
-    
 
+    $(window).scrollTop(0);
+    
     $(window).scroll(function() {
         if ($(document).scrollTop() > 10 && headerclosed == false) {
-            $(".header").addClass("sticky");
             headerclosed = true;
             $(".elemname").fadeOut(300);
             $(".element").animate({width: '4.4rem', height: '4.5rem', fontSize: '0.8rem'}, 500, "linear");
-            $(".elemnum, .elemmass").slideDown(250);
+            $(".elemnum, .elemmass").animate({fontSize: '1rem'});
         } else if ($(document).scrollTop() < 5 && headerclosed == true) {
-            $(".header").removeClass("sticky");
             headerclosed = false;
-            $(".elemnum, .elemmass").slideUp(300)
+            $(".elemnum, .elemmass").animate({fontSize: '0rem'});
             $(".element").animate({width: '17rem', height: '5rem', fontSize: '1.8rem'}, 300);
             $(".elemname").fadeIn();
         }
