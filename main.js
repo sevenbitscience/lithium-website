90070ea0e41155a470e91b02e034c538a08e34bb
sevenbitscience <milausnic@gmail.com>
2021-10-28 10:01:01 -0400

    element colapes on scroll

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
 $(document).ready(function () {
-    $(".elemname").hide(0);
-    
-    var offset = $(".header").offset();
-    var top = offset.top;
-    var left = offset.left;
     var headerclosed = false;
-    
+
+    $(window).scrollTop(0);
 
     $(window).scroll(function() {
-        if ($(document).scrollTop() > 10 && headerclosed == false) {
-            $(".header").addClass("sticky");
+        if ($(document).scrollTop() > 3 && headerclosed == false) {
             headerclosed = true;
             $(".elemname").fadeOut(300);
-            $(".element").animate({width: '4.4rem', height: '4.5rem', fontSize: '0.8rem'}, 500, "linear");
-            $(".elemnum, .elemmass").slideDown(250);
-        } else if ($(document).scrollTop() < 5 && headerclosed == true) {
-            $(".header").removeClass("sticky");
+            $(".element").animate({fontSize: '0.8rem', width: '4.4rem', height: '4.2rem'}, 300, "linear");
+            $(".content").animate({paddingTop: '8.5rem'}, 300);
+            $(".elemnum, .elemmass").animate({fontSize: '0.8rem'}, 50);
+        } else if ($(document).scrollTop() < 4 && headerclosed == true) {
             headerclosed = false;
-            $(".elemnum, .elemmass").slideUp(300)
-            $(".element").animate({width: '17rem', height: '5rem', fontSize: '1.8rem'}, 300);
+            $(".elemnum, .elemmass").animate({fontSize: '0rem'}, 100);
+            $(".element").animate({width: '18rem', height: '5.2rem', fontSize: '2rem'}, 300);
+            $(".content").animate({paddingTop: '9rem'}, 300);
             $(".elemname").fadeIn();
         }
       });
