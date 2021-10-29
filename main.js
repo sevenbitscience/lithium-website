90aef9f9e0a12aec6cf7742f536a7abacb31ffee
sevenbitscience <milausnic@gmail.com>
2021-10-29 08:31:35 -0400

    finish element animation and update stylesheet

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ $(document).ready(function () {
             headerclosed = true;
             $(".elemname").fadeOut(300);
             $(".element").animate({fontSize: '0.8rem', width: '4.4rem', height: '4.2rem'}, 300, "linear");
-            $(".content").animate({paddingTop: '8.5rem'}, 300);
+            $(".content").animate({marginTop: '8.5rem'}, 200);
             $(".elemnum, .elemmass").animate({fontSize: '0.8rem'}, 50);
         } else if ($(document).scrollTop() < 4 && headerclosed == true) {
             headerclosed = false;
             $(".elemnum, .elemmass").animate({fontSize: '0rem'}, 100);
             $(".element").animate({width: '18rem', height: '5.2rem', fontSize: '2rem'}, 300);
-            $(".content").animate({paddingTop: '9rem'}, 300);
+            $(".content").animate({marginTop: '9rem'}, 200);
             $(".elemname").fadeIn();
         }
       });
